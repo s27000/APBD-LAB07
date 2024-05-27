b52a05cd101517b34664344dc47eb867c9c038f7
PC <s27000@pjwstk.edu.pl>
2024-05-27 20:37:32 +0200

    git upload

---
+++ LinqCwiczenia/LinqTutorials/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var t = LinqTasks.Task1();
+            //var t = LinqTasks.Task2();
+            //Console.WriteLine(LinqTasks.Task3());
+            //var t = LinqTasks.Task4();
+            //var t = LinqTasks.Task5();
+            //var t = LinqTasks.Task6();
+            //var t = LinqTasks.Task7();
+            //Console.WriteLine(LinqTasks.Task8());
+            //Console.WriteLine(LinqTasks.Task9());
+            //var t = LinqTasks.Task10();
+            //var t = LinqTasks.Task11();
+            //var t = LinqTasks.Task12();
+
+            //int[] arr = { 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1 };
+            //Console.WriteLine(LinqTasks.Task13(arr));
+
+            //var t = LinqTasks.Task14();
+            //var t = LinqTasks.Task15();
+            var t = LinqTasks.Task16();
+            ///*
+            foreach (var it in t)
+            {
+                Console.WriteLine(it);
+            }
+            //*/
+        }
+    }
+}
